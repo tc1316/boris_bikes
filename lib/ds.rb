640a06a732ddf1a33f1f0501a74bd056b6f47442
WaqasIkram <waqaschib@gmail.com>
2021-11-17 15:35:33 +0000

    Use of instance variables

--- lib/ds.rb
+++ lib/ds.rb
@@ -1,7 +1,17 @@
 require_relative "./bike.rb"
 
 class DockingStation
+    attr_reader :bikelist
+    
+    def initialize 
+        @bikelist = []
+    end
+    
     def release_bike
         bike = Bike.new
+    end
+    
+    def dock(x)
+        bikelist << x 
     end
 end
